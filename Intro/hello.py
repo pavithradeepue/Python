43b1bd2d9e607ec6e72d60f689b092f3f079c1fa
Pavithra Deepu E <pavithradeepue2005@gmail.com>
2025-03-22 23:43:28 +0530

    Update and rename helloworld.py to Intro/hello.py

---
+++ Intro/hello.py
@@ -0,0 +1,43 @@
+print("I love burger")
+print("Its really good")
+name = "Pavithra"
+age = 19
+gpa = 8.8
+is_student = True
+
+print(f"Hello {name}")
+print(f"Your age is {age}")
+print(f"Your gpa is{gpa}")
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT a student")
+
+# Learnt Arithemetic Operators
+frnd = 10
+frnd = frnd//3
+print(frnd)
+
+f = frnd % 3
+print(f)
+
+#Type Casting
+print(type(gpa))
+gpa = int(gpa)
+print(gpa)
+
+
+#OUTPUT
+'''
+python hello.py
+I love burger
+Its really good
+Hello Pavithra
+Your age is 19
+Your gpa is8.8
+You are a student
+3
+0
+<class 'float'>
+8
+'''
